13b2b798254acf9a8b8846d482c967adace8e4cc
Casca Kwok <casca.kwok@gmail.com>
2022-02-06 10:09:41 -0800

    Update corona_daily_case_plot.py

--- corona_daily_case_plot.py
+++ corona_daily_case_plot.py
@@ -2,7 +2,7 @@
 import requests
 
 from pylab import * #matplotlib
-from scipy.ndimage.filters import gaussian_filter1d #To smooth y axis
+from scipy.ndimage import gaussian_filter1d #To smooth y axis
 import matplotlib.style as style #beautify graph
 #import mpl_font.wqy #plot with chinese titles
 
@@ -25,10 +25,18 @@ def corona_daily_case_plot():
     daily_corona_case_num = requests.request("GET", url, headers=headers, data = payload).json()
 
     for i in daily_corona_case_num:
+
         dates.append(i["As of date"])
-        y.append(i["Number of confirmed cases"])
-        delta.append(i["Number of confirmed cases"] - previous_cases)
-        previous_cases = i["Number of confirmed cases"]
+
+        if i["Number of confirmed cases"] != '':
+            y.append(i["Number of confirmed cases"])
+            delta.append(i["Number of confirmed cases"] - previous_cases)
+            previous_cases = i["Number of confirmed cases"]
+        else:
+            y.append(i["Number of cases tested positive for SARS-CoV-2 virus"])
+            delta.append(i["Number of cases tested positive for SARS-CoV-2 virus"] - previous_cases)
+            previous_cases = i["Number of cases tested positive for SARS-CoV-2 virus"]
+
 #
 
     daily_num_of_cases = str(y[-1]) + "宗"
@@ -37,6 +45,15 @@ def corona_daily_case_plot():
     x = [datetime.datetime.strptime(d,"%d/%m/%Y").date() for d in dates]
 #    plottitle = "Confirmed Convid-19 Cases - HK Daily Trend" + "\n" + dates[-1] + " : " + str(y[-1]) + " Cases"
 
+    """
+    title('香港新型肺炎確診宗數', fontsize=18)
+    xlabel('日期', fontsize=18)
+    ylabel('宗數', fontsize=18)
+    """
+
+#    title(plottitle, fontsize=32)
+#    xlabel('Date', fontsize=28)
+#    ylabel('Number of cases', fontsize=28)
     ysmoothed = gaussian_filter1d(y, sigma=2)
     plt.plot(x, ysmoothed)
     grid(True)
@@ -52,4 +69,5 @@ def corona_daily_case_plot():
     plt.close()
 
     return daily_num_of_cases, last_update
+
 
